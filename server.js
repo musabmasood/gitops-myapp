de0b45bbcab0c1b1dad8aec469391698d36b6b74
Musab Mirza <8170066+musabmasood@users.noreply.github.com>
2020-05-18 16:26:30 -0400

    added color variable

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var http = require('http');
 const fs = require("fs")
+const color = "red" 
 
 version = (fs.readFileSync("/VERSION", 'utf8')).toString()
 console.log("Starting MyApp version " + version)
 http.createServer(function (req, res) {
   res.write('<h1>GitOps MyApp</h1>');
-  res.write('<h2 style="color:purple">Version '+version+'</h2>');
+  res.write('<h2 style="color:'+color+'">Version '+version+'</h2>');
   res.end(); 
 }).listen(8080); 
