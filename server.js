e433c82688d661c178f34842fc9eedeef6da4805
Musab Mirza <8170066+musabmasood@users.noreply.github.com>
2020-05-18 15:55:23 -0400

    changed color

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ version = (fs.readFileSync("/VERSION", 'utf8')).toString()
 console.log("Starting MyApp version " + version)
 http.createServer(function (req, res) {
   res.write('<h1>GitOps MyApp</h1>');
-  res.write('<h2 style="color:green">Version '+version+'</h2>');
+  res.write('<h2 style="color:purple">Version '+version+'</h2>');
   res.end(); 
 }).listen(8080); 
